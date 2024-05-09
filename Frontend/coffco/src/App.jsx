0ebc1359fa64128006afe2d0f7e2e5821ed1b546
Lumaryy <luisamariasancheztovar2006@gmail.com>
2024-05-09 13:38:21 -0500

    proyectoReact

---
+++ Frontend/coffco/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+//importar pages
+
+
+import {Route,Routes} from "react-router-dom"
+
+//importar components
+
+const App = () => {
+ return (
+ <>
+<Routes>
+
+</Routes>
+ </>
+ );
+};
+export default App;
